c309171b338a44bbd6572cd0973c1486ad031425
Bikash Rajguru <bikashrajguru@fb.com>
2019-11-12 14:46:11 +0800

    avoid going back to main screen

--- app/src/main/java/com/example/shredgrind/MainActivity.java
+++ app/src/main/java/com/example/shredgrind/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(intent);
+                finish();
             }
         }, 5000);
 
